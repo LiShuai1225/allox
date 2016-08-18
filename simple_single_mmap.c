8e6eb84fb7cd158937317eef24018de836c18952
Lorenzo Stoakes <lstoakes@gmail.com>
2016-08-18 09:51:06 +0100

    munmap too.

--- simple_single_mmap.c
+++ simple_single_mmap.c
@@ -5,6 +5,12 @@
 
 #include "allox.h"
 
+static void fatal_perror(void)
+{
+	perror("simple_single_mmap");
+	exit(1);
+}
+
 int main(int argc, char *argv[])
 {
 	int i;
@@ -12,12 +18,10 @@ int main(int argc, char *argv[])
 
 	parse_args(argc, argv);
 
-	ptr = mmap(NULL, HPAGES * HPAGE_SIZE, PROT_READ | PROT_WRITE,
+	ptr = mmap(NULL, SIZE, PROT_READ | PROT_WRITE,
 		MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	if (ptr == MAP_FAILED) {
-		perror("simple_single_mmap");
-		exit(1);
-	}
+	if (ptr == MAP_FAILED)
+		fatal_perror();
 
 	/* Touch each 4KiB page. */
 	for (i = 0; i < TOTAL_PAGES; i++)
@@ -25,6 +29,9 @@ int main(int argc, char *argv[])
 
 	summarise(ptr);
 	block();
+
+	if (munmap(ptr, SIZE))
+		fatal_perror();
 
 	return EXIT_SUCCESS;
 }
