17c176a17e3f700de2e4958f1d2d9cb68d6046e4
Lorenzo Stoakes <lstoakes@gmail.com>
2016-08-18 09:39:06 +0100

    Vertically align.

--- allox.h
+++ allox.h
@@ -10,7 +10,7 @@
 #define PAGES_PER_HPAGE (HPAGE_SIZE/PAGE_SIZE)
 
 /* Number of huge page-blocks to allocate. */
-#define HPAGES 42UL
+#define HPAGES      42UL
 #define TOTAL_PAGES (HPAGES * PAGES_PER_HPAGE)
 
 /* helpers.c */
