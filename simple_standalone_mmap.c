cc984c1efbe58badcb0d3480354cdbc866d2a002
Lorenzo Stoakes <lstoakes@gmail.com>
2016-09-15 08:49:08 +0100

    Expand standalone mmap to put some data into the memory.

--- simple_standalone_mmap.c
+++ simple_standalone_mmap.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <unistd.h>
 
@@ -9,12 +10,23 @@
 
 int main(void)
 {
-	void *ptr = mmap(NULL, SIZE, PROT_READ | PROT_WRITE,
-		MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	unsigned char *ptr = mmap(NULL, SIZE, PROT_READ | PROT_WRITE,
+				MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	if (ptr == MAP_FAILED) {
 		perror("mmap");
 		exit(1);
 	}
+
+	puts("<hit ret>");
+	getchar();
+
+	ptr[0] = 'x';
+	ptr[SIZE-1] = 'y';
+
+	puts("<hit ret>");
+	getchar();
+
+	memset(ptr, 'x', SIZE);
 
 	puts("<hit ret>");
 	getchar();
